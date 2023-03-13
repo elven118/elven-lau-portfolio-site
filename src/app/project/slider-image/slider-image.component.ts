eb95c46e21991fbf4052c79f227b8261addc1d10
elven118 <elven118@gmail.com>
2023-03-13 14:28:02 -0400

    Update project page with image carousel

--- src/app/project/slider-image/slider-image.component.ts
+++ src/app/project/slider-image/slider-image.component.ts
@@ -1,25 +1,48 @@
-import { Component, Input } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  Input,
+  QueryList,
+  ViewChild,
+  ViewChildren,
+} from '@angular/core';
 
 @Component({
   selector: 'app-slider-image',
   templateUrl: './slider-image.component.html',
-  styleUrls: ['./slider-image.component.scss']
+  styleUrls: ['./slider-image.component.scss'],
 })
 export class SliderImageComponent {
   @Input() imagePaths: string[] = [];
+  @ViewChild('imageSlide') private imageSlideRef: ElementRef | undefined;
+  // For Image container width
+  @ViewChildren('image') private imageRefs: QueryList<ElementRef> =
+    new QueryList();
   currentIndex = 0;
+  currImgWidth = 0;
 
-  handlePrev = () => {
-    this.currentIndex = this.currentIndex-1;
-    if (this.currentIndex < 0) {
-      this.currentIndex = this.imagePaths.length-1;
-    }
+  onFirstImageLoad() {
+    this.handleChangePage(0);
   }
 
-  handleNext = () => {
-    this.currentIndex = this.currentIndex+1;
+  handleChangePage = (i: number) => {
+    this.currentIndex = i;
+    if (this.currentIndex < 0) {
+      this.currentIndex = this.imagePaths.length - 1;
+    }
     if (this.currentIndex > this.imagePaths.length - 1) {
       this.currentIndex = 0;
     }
+    this.imageSlideRef!.nativeElement.style.transform =
+      'translateX(' +
+      -this.imageRefs.reduce(
+        (sum, next, curIndex) =>
+          curIndex < this.currentIndex ? sum + next.nativeElement.width : sum,
+        0
+      ) +
+      'px)';
+    this.currImgWidth = this.imageRefs.get(
+      this.currentIndex
+    )?.nativeElement.width - 1;
   }
 }
